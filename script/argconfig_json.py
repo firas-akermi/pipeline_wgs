a87e6ec57c47a2d31f8d76093bb977841d817fca
fakermi <firas.arkemi-ext@aphp.fr>
2022-08-19 12:37:07 +0000

    update rules

--- script/argconfig_json.py
+++ script/argconfig_json.py
@@ -296,7 +296,7 @@ if __name__ == '__main__':
     parser.add_argument("-nvcfhg002","--vcfhg002_name",help="standard hg002 VCf file name", type = str, required= False)
     parser.add_argument("-nbedhg002","--bedhg002_name",help="standard hg002 bed file name", type = str, required= False)
     parser.add_argument("-pbedhg002","--pbedhg002_path",help="standard hg002 bed file path", type = str, required= False)
-    parser.add_argument("-query_sv_path","--vcf_query_sv_path",help="path to  vcf query for witty", type = str, required= True)
+    parser.add_argument("-query_sv_path","--vcf_query_sv_path",help="path to  vcf query for witty", type = str, required= False)
     parser.add_argument("-query_sv_name","--vcf_query_sv_name",help="query vcf name for witty", type = str, required= False)
     parser.add_argument("-Enviro","--Environnement",help="Calculation environment", type = str, required= True)
     parser.add_argument("-tool","--tools", help="tool to run Hapy, ClinSV or Witty",type = str, required= True)
